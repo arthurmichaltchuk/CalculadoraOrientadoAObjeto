d7c98e16039043d436e9cad0e62534cdc15fd6ee
arthur michaltchuk <arthurmichaltchuk@uniplac.edu.br>
2021-04-21 12:44:31 -0300

    first commit

---
+++ CalculadoraOrientado/RoboOrientado.ConsoleApp/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboOrientado.ConsoleApp
+{
+    public class Calculadora
+    {
+        string operador;
+        int cont;
+        string[] hist = new string[100];
+        public double numero1;
+        public double numero2;
+        internal void Somar(double numero1, double numero2, out double resultado)
+        {
+            resultado = numero1 + numero2; 
+            operador = "+";
+            Registrador(operador, resultado);
+        }
+        internal void Subtrair(double numero1, double numero2, out double resultado)
+        {
+            resultado = numero1 - numero2;
+            operador = "-";
+            Registrador(operador, resultado);
+        }
+        internal void Multiplicar(double numero1, double numero2, out double resultado)
+        {
+            resultado = numero1 * numero2;
+            operador = "*";
+            Registrador(operador, resultado);
+        }
+        internal void Dividir(double numero1, double numero2, out double resultado)
+        {
+            resultado = numero1 / numero2;
+            operador = "/";
+            if (numero2 != 0)
+                Registrador(operador, resultado);
+            else
+                Console.Write("ERR");resultado = 0;
+        }
+        public void Registrador(string op, double resultado)
+        {
+            hist[cont] = $"{numero1} {op} {numero2} = {resultado}";
+            cont++;
+        }
+        public void Visualizar()
+        {
+            if (hist[0] == null)
+            {
+                Console.WriteLine("Histórico vazio...");
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (hist[i] != null)
+                    {
+                        Console.WriteLine(hist[i]);
+                    }
+                }
+            }      
+        }
+    }
+}
---
+++ CalculadoraOrientado/RoboOrientado.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboOrientado.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora operacao1 = new Calculadora();
+            double resultado;
+            string opcao;
+            resultado = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("...Calculadora 1.0...\n1. Para somar........\n2. Para subtrair.....\n3. Para multiplicar..\n4. Para dividir......\n5. Para ver histórico\n6. Para sair.........\n");
+                opcao = Console.ReadLine();
+        
+                switch (opcao)
+                {
+                    case "1":
+                        InserirNumeros(operacao1);
+                        operacao1.Somar(operacao1.numero1, operacao1.numero2, out resultado); Console.WriteLine(resultado); break;
+                    case "2":
+                        InserirNumeros(operacao1);
+                        operacao1.Subtrair(operacao1.numero1, operacao1.numero2, out resultado); Console.WriteLine(resultado); break;                 
+                    case "3":
+                        InserirNumeros(operacao1);
+                        operacao1.Multiplicar(operacao1.numero1, operacao1.numero2, out resultado); Console.WriteLine(resultado); break;                  
+                    case "4":
+                        InserirNumeros(operacao1);
+                        operacao1.Dividir(operacao1.numero1, operacao1.numero2, out resultado); Console.WriteLine(resultado); break;
+                    case "5":operacao1.Visualizar();break;
+                    case "6":break;
+                    default:Console.WriteLine("Opção inválida! Tente novamente");break; 
+                }
+                Console.ReadLine();
+            } while (opcao != "6");         
+        }
+
+        private static void InserirNumeros(Calculadora operacao1)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            operacao1.numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            operacao1.numero2 = Convert.ToDouble(Console.ReadLine());
+        }
+    } 
+}
